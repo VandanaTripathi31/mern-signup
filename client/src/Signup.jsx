704cc46119bcc3dfa82ff8568952d4e243b6572b
Vandana Tripathi <146467007+VandanaTripathi31@users.noreply.github.com>
2024-11-19 19:44:50 +0530

    Update Signup.jsx

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -12,7 +12,7 @@ const Signup = () => {
 
     const handeleSubmit = (e) => {
         e.preventDefault()
-        axios.post('http://localhost:3001/register', {name, email,password})
+        axios.post('https://mern-signup-api.vercel.app/register', {name, email,password})
         .then(result => console.log(result))
         navigate('/login')
         
