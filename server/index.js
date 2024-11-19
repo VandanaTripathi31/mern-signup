599b87cf357d5bd9026bbfa74f0a48bdf987f0ba
Vandana Tripathi <146467007+VandanaTripathi31@users.noreply.github.com>
2024-11-19 19:51:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 {
-    origin: [""]
+    origin: ["https://mern-signup-fronend.vercel.app/"]
     methods: ["POST", "GET"]
     credentials: true
 }
