aa0f9fb20ed29b37398dc8e3aa28fa00a3a044f4
Vandana Tripathi <146467007+VandanaTripathi31@users.noreply.github.com>
2024-11-19 20:28:28 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,31 @@
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Signup from './Signup'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Login from './Login'
-import Home from './Home'
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './Signup';
+import Login from './Login';
+import Home from './Home';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
-
   return (
-    <BrowserRouter future={{ v7_relativeSplatPath: true }}>
-    <Routes>
-      <Route path='/*' element={<Signup/>}></Route>
-      <Route path='login/*' element={<Login/>}></Route>
-      <Route path='home/*' element={<Home/>}></Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        {/* Default route to Signup */}
+        <Route path="/" element={<Navigate to="/signup" />} />
+        
+        {/* Signup Route */}
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Login Route */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Home Route */}
+        <Route path="/home" element={<Home />} />
+
+        {/* 404 - Page Not Found */}
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
